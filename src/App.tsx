aa8cc7e09c0fc744d0d1248e08b5e84396cb4591
Akane Shoji <cahierreco+dev@gmail.com>
2021-01-25 17:57:02 +0900

    減色処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
 
   const [grid, setGrid] = useState(32)
   const [gridSwitch, setGridSwitch] = useState(0)
+  const [bright, setBright] = useState(27)
+  const [brightSwitch, setBrightSwitch] = useState(0)
 
   const handleGrid = (hIEvent: React.ChangeEvent<HTMLInputElement>) => {
     if(hIEvent.target.value != null){
@@ -28,6 +30,26 @@ function App() {
     }
   }
 
+  const handleBright = (hIEvent: React.ChangeEvent<HTMLInputElement>) => {
+    if(hIEvent.target.value != null){
+      var brightStr = hIEvent.target.value
+      var brightNum: number = +brightStr
+      setBright(brightNum)
+    }
+  }
+
+  const switchBright = (hIEvent: React.ChangeEvent<HTMLInputElement>) => {
+    if(hIEvent.target.value != null){
+      var brightSwitchStr = hIEvent.target.value
+      var brightSwitchNum: number = +brightSwitchStr
+      if(brightSwitchNum === 0){
+        brightSwitchNum = 1
+      }else{
+        brightSwitchNum = 0
+      }
+      setBrightSwitch(brightSwitchNum)
+    }
+  }
 
   const handleImage = (hIEvent: React.ChangeEvent<HTMLInputElement>) => {
     if(hIEvent.target.files != null){
@@ -67,7 +89,7 @@ function App() {
     const canvasB = document.getElementById("postCanvas") as HTMLCanvasElement
     const ctxA =  canvasA.getContext('2d')
     const ctxB =  canvasB.getContext('2d')
-
+    
     if(ctxA != null && ctxB != null){
       var gridSize = canvasA.width / grid
       
@@ -81,15 +103,33 @@ function App() {
           var pAlpha = 0
 
           for (var i = 0; i < data.length; i += 4) {
-            pRed     += data[i]   // red
-            pGreen   += data[i + 1] // green
-            pBlue    += data[i + 2] // blue
-            pAlpha   += data[i + 3] // alpha
+              pRed     += data[i]  // red
+              pGreen   += data[i + 1]  // green
+              pBlue    += data[i + 2]  // blue
+              pAlpha   += data[i + 3] // alpha
           }
-          pRed = pRed / (data.length / 4)
-          pGreen = pGreen / (data.length / 4)
-          pBlue = pBlue / (data.length / 4)
-          pAlpha = pAlpha / (data.length / 4)
+          if(brightSwitch === 0){
+            pRed = pRed / (data.length / 4)
+            pGreen = pGreen / (data.length / 4)
+            pBlue = pBlue / (data.length / 4)
+            pAlpha = pAlpha / (data.length / 4)
+          }else{
+            if(bright == 8){
+              pRed = reduceColor8(pRed / (data.length / 4))
+              pGreen = reduceColor8(pGreen / (data.length / 4))
+              pBlue = reduceColor8(pBlue / (data.length / 4))
+            }else if(bright == 27){
+              pRed = reduceColor27(pRed / (data.length / 4))
+              pGreen = reduceColor27(pGreen / (data.length / 4))
+              pBlue = reduceColor27(pBlue / (data.length / 4))
+            }else{
+              pRed = reduceColor64(pRed / (data.length / 4))
+              pGreen = reduceColor64(pGreen / (data.length / 4))
+              pBlue = reduceColor64(pBlue / (data.length / 4))
+            }
+
+            pAlpha = pAlpha / (data.length / 4)            
+          }
 
           //var pColor = (pRed + pBlue + pGreen) / 3
 
@@ -105,11 +145,47 @@ function App() {
         ctxB.putImageData(imageData,m * gridSize,n * gridSize)
         }
       }
-      if(gridSwitch === 0){
+      if(gridSwitch === 1){
         drawGrid()
       }
       //
     }
+  }
+
+  const reduceColor8 = (pictData: number) => {
+    var result = 0
+    if(pictData < 128){
+      result = 64
+    }else{
+      result = 192
+    }
+    return result
+  }
+
+  const reduceColor27 = (pictData: number) => {
+    var result = 0
+    if(pictData < 85){
+      result = 43
+    }else if(pictData < 170){
+      result = 128
+    }else{
+      result = 213
+    }
+    return result
+  }
+
+  const reduceColor64 = (pictData: number) => {
+    var result = 0
+    if(pictData < 64){
+      result = 0
+    }else if(pictData < 128){
+      result = 96
+    }else if(pictData < 192){
+      result = 160
+    }else{
+      result = 224
+    }
+    return result
   }
 
   const drawGrid = () => {
@@ -140,15 +216,28 @@ function App() {
       <h1>ドット絵変換ジェネレータ</h1>
       <input type="file" id="pict" onChange={handleImage} />
 
+      <br />
+      <input type="checkbox" id="gridSwitch" name="gridSwitch" value={gridSwitch} onChange={switchGrid} checked={gridSwitch === 1} />
+      <label>グリッドを描画する</label>
+
       <input type="radio" name="grid" value="16" onChange={handleGrid} checked={grid === 16} />
       <label>16x16</label>
       <input type="radio" name="grid" value="32" onChange={handleGrid} checked={grid === 32} />
       <label>32x32</label>
       <input type="radio" name="grid" value="64" onChange={handleGrid} checked={grid === 64} />
       <label>64x64</label>    
+      
       <br />
-      <input type="checkbox" id="gridSwitch" name="gridSwitch" value={gridSwitch} onChange={switchGrid} checked={gridSwitch === 0} />
-      <label>グリッドを描画する</label>
+      <input type="checkbox" id="switchBright" name="switchBright" value={brightSwitch} onChange={switchBright} checked={brightSwitch === 1} />
+      <label>減色処理をONにする</label>
+
+      <input type="radio" name="bright" value="8" onChange={handleBright} checked={bright === 8} />
+      <label>8色</label>
+      <input type="radio" name="bright" value="27" onChange={handleBright} checked={bright === 27} />
+      <label>27色</label>
+      <input type="radio" name="bright" value="64" onChange={handleBright} checked={bright === 64} />
+      <label>64色</label>  
+
       <br />
       <input type="button" value="ドットに変換！！" onClick={pixelize}/>
       <br />
